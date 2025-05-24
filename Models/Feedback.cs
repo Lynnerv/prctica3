0cd4cbed98a7f58f7c81ccb27c2b2009785bc118
Lynnerv <aythor_briceno@usmp.pe>
2025-05-23 21:59:19 -0500

    creacionModels2

--- Models/Feedback.cs
+++ Models/Feedback.cs
@@ -7,6 +7,9 @@ namespace prctica3.Models
 {
     public class Feedback
     {
-        
+    public int Id { get; set; }
+    public int PostId { get; set; }
+    public string Sentimiento { get; set; } // "like" o "dislike"
+    public DateTime Fecha { get; set; }
     }
 }
